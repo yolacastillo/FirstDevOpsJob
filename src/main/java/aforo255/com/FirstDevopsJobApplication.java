6d65499bf42e04e737738dea921405e2d486c7a6
Stywar <franco111988@gmail.com>
2021-02-04 01:39:27 -0500

    sea grego war

--- src/main/java/aforo255/com/FirstDevopsJobApplication.java
+++ src/main/java/aforo255/com/FirstDevopsJobApplication.java
@@ -2,10 +2,18 @@ package aforo255.com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class FirstDevopsJobApplication {
+public class FirstDevopsJobApplication extends SpringBootServletInitializer {
 
+	// Agregamos esto metodos 
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(FirstDevopsJobApplication.class);
+    }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(FirstDevopsJobApplication.class, args);
 	}
