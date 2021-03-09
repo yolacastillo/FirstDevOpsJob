23f47b2b9208371ec289db7251bdf977a02fb618
Stywar <franco111988@gmail.com>
2021-03-09 02:23:05 -0500

    roles

--- configuration/iaac/aws/kubernetes/main.tf
+++ configuration/iaac/aws/kubernetes/main.tf
@@ -78,3 +78,29 @@ resource "kubernetes_cluster_role_binding" "example" {
 provider "aws" {
   region  = "us-east-1"
 }
+
+resource "aws_iam_role" "ec2_iam_role" {
+  name = "ec2_iam_role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": [
+          "ec2.amazonaws.com"
+        ]
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "ec2-read-only-policy-attachment" {
+    role = "${aws_iam_role.ec2_iam_role.name}"
+    policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ReadOnlyAccess"
+}
