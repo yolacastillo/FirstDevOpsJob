860cc5f490563d5b587a335cb7105dacd4975317
Stywar <franco111988@gmail.com>
2021-03-09 19:19:43 -0500

    se agrego cambios

--- src/main/java/aforo255/com/controller/TestController.java
+++ src/main/java/aforo255/com/controller/TestController.java
@@ -8,6 +8,6 @@ public class TestController {
 
 	@GetMapping("/test")
 	public String TestFirstJob() {
-		return "Welcome to MasterDevops |Tomcat v1 | Ansible , AFORO255";
+		return "Welcome to MasterDevops |Tomcat v1345 | Ansible , AFORO255";
 	}
 }
