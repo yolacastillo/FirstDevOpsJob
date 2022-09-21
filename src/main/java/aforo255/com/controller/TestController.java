fa791678c820826654f5b21e95b72ee255bd2bf0
Yolanda Castillo <castillo.yola@gmail.com>
2022-09-20 21:36:20 -0500

    Update TestController.java

--- src/main/java/aforo255/com/controller/TestController.java
+++ src/main/java/aforo255/com/controller/TestController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class TestController {
+public class TestController { //2022-09-20
 
 	@GetMapping("/test")
 	public String TestFirstJob() {
