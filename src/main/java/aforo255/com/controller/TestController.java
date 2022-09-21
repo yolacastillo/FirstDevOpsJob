9476092d891f991b683abcb1ae48a8e50522047a
Yolanda Castillo <castillo.yola@gmail.com>
2022-09-20 21:47:14 -0500

    Update TestController.java

--- src/main/java/aforo255/com/controller/TestController.java
+++ src/main/java/aforo255/com/controller/TestController.java
@@ -8,6 +8,6 @@ public class TestController { //2022-09-20
 
 	@GetMapping("/test")
 	public String TestFirstJob() {
-		return "Welcome to MasterDevops |Tomcat v1345 | Ansible , AFORO255";
+		return "2 Welcome to MasterDevops |Tomcat v1345 | Ansible , AFORO255";
 	}
 }
